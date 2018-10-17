a849f727620e6ea68eb73cd86a8927757e8e03bd
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-16 18:19:39 -0700

    Finished challenges 3 and 4, getting silly with variable names now

--- 301/passByRefOrVal/challenges-02.test.js
+++ 301/passByRefOrVal/challenges-02.test.js
@@ -48,6 +48,7 @@ console.log(a) prints { fullName: 'Octavia Estelle Butler', yearBorn: 1947 }
 
 const addBirthYearProperty = (obj, year) => {
   // Solution code here...
+  obj.yearBorn = year;
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -65,6 +66,9 @@ console.log(people[1].isAuthor) prints true
 
 const setStatusAsAuthor = (people) => {
   // Solution code here...
+  for(let writers = 0; writers < people.length; writers++){
+    people[writers].isAuthor = true;
+  }
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -117,7 +121,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should add a property to an object', () => {
     const a = { fullName: 'Octavia Butler' };
     addBirthYearProperty(a, 1947);
@@ -126,7 +130,7 @@ xdescribe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should add a property to every object in an array', () => {
     const a = [{ fullName: 'Octavia Butler' }, { fullName: 'Ray Bradbury' }, { fullName: 'Kurt Vonnegut' }];
     setStatusAsAuthor(a);
@@ -137,7 +141,7 @@ xdescribe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should append the second array to the first', () => {
     const a = [1, 2, 3, 4];
     const b = [5, 6, 7, 8];
