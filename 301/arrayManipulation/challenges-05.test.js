f50179e6562f2e01013d6698753280ffc9af894d
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-21 16:07:44 -0700

    strike the previous commit, didn't actually activate the test

--- 301/arrayManipulation/challenges-05.test.js
+++ 301/arrayManipulation/challenges-05.test.js
@@ -275,7 +275,7 @@ describe('Testing challenge 5', () => {
   });
 });
 
-xdescribe('Testing challenge 6', () => {
+describe('Testing challenge 6', () => {
   test('It should remove the even numbers from the array', () => {
     let list = [1, 2, 3, 4, 5, 6];
     removeEvenValues(list);
