600f67aac76a5d155e18bfb8d835134a11c9f43d
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-23 18:52:37 -0700

    Finished challenge 5 - I think that was as much code as I wrote in the first 4 challenges

--- 301/arrayMap/challenges-07.test.js
+++ 301/arrayMap/challenges-07.test.js
@@ -57,9 +57,8 @@ For example: charCode(['h','i']) returns [104, 105].
 
 const charCode = (arr) => {
   // Solution code here...
-  // console.log(arr);
-  // let arrCode = arr;
-  return arr.map(charElement => charElement.charCodeAt(0));
+  let arrCode = arr;
+  return arrCode.map(charElement => charElement.charCodeAt(0));
 
 }
 
@@ -75,6 +74,18 @@ For example: evenOdd([1,2,3]) returns ['odd','even','odd'].
 
 const evenOdd = (arr) => {
   // Solution code here...
+  return arr.map(num => {
+    let parity = 'N/A';
+    if(typeof(num) === 'number'){
+      if(num % 2){
+        parity = 'odd';
+      }
+      else{
+        parity = 'even';
+      }
+    }
+    return parity;
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -221,7 +232,7 @@ describe('Testing challenge 4', () => {
   });
 });
 
-xdescribe('Testing challenge 5', () => {
+describe('Testing challenge 5', () => {
   test('It should return an array containing the keys from an object', () => {
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541])).toStrictEqual([ 'odd', 'even', 'even', 'even', 'odd', 'odd', 'even', 'odd' ]);
     expect(evenOdd([5, 8, 2, 6, 9, 13, 542, 541]).length).toStrictEqual(8);
