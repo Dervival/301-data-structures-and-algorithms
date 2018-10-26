049b51a895ccac254690e8ce5eba4a9c7627fa27
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-26 13:43:47 -0700

    Finished challenge 3

--- 301/arrayReduce/challenges-09.test.js
+++ 301/arrayReduce/challenges-09.test.js
@@ -90,6 +90,13 @@ Note: You must use reduce for this challenge. You may not use the built-in .reve
 
 const reversedString = (arr) => {
   // Solution code here...
+  let arrArray = arr.split('');
+  console.log(arrArray)
+  let reversedArray = arrArray.reduce((newArray, character, index) =>{
+    newArray.push(arrArray[(arrArray.length-1)-index]);
+    return newArray;
+  }, []);
+  return reversedArray.join('');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -258,7 +265,7 @@ describe('Testing challenge 2', () => {
   });
 });
 
-xdescribe('Testing challenge 3', () => {
+describe('Testing challenge 3', () => {
   test('It should return the string with the characters in reverse order', () => {
     expect(reversedString('Code 301')).toStrictEqual('103 edoC');
   });
