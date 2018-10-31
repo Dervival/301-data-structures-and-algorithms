804def03e6576c8c58ce38fce6ec8df0709935c5
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-31 13:55:48 -0700

    Finished challenge 2

--- 301/stringManipulation/challenges-13.test.js
+++ 301/stringManipulation/challenges-13.test.js
@@ -25,6 +25,9 @@ For example, ['this is great :)', 'wow', 'whyyyyyy :(', ':)))))'] returns ['this
 
 const findHappiness = (arr) => {
   // Solution code here...
+  return arr.filter( string =>{
+    return string.includes(':)');
+  })
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -157,7 +160,7 @@ describe('Testing challenge 1', () => {
   });
 });
 
-xdescribe('Testing challenge 2', () => {
+describe('Testing challenge 2', () => {
   test('It should return only the strings that contain smiley faces', () => {
     const words = ['things', 'apple (:)', ':)banana', 'missing that thing', 'cant:)aloupe'];
 
