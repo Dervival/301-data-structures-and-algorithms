59ad0cce558ae234d94257eec10c15971f9bfecb
Blaise Clarke <blaiserclarke@gmail.com>
2018-10-31 14:24:37 -0700

    Finished challenge 4

--- 301/stringManipulation/challenges-13.test.js
+++ 301/stringManipulation/challenges-13.test.js
@@ -191,7 +191,7 @@ describe('Testing challenge 3', () => {
   });
 });
 
-xdescribe('Testing challenge 4', () => {
+describe('Testing challenge 4', () => {
   test('It should only return the odd indexed characters from the string', () => {
     expect(onlyOddChars('0123456789')).toStrictEqual('13579');
     expect(onlyOddChars('abcd')).toStrictEqual('bd');
